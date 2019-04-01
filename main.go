f051a23fafb9675b6ba0273f43dfda663c8bb92e
阿部直也 <n.abe@gemcook.com>
2019-04-01 20:21:48 +0900

    [update] 削除機能修正完了

--- main.go
+++ main.go
@@ -22,27 +22,26 @@ type Result struct {
 // DB migration
 func dbInit() {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbInit())")
 	}
 	db.AutoMigrate(&Book{})
+	defer db.Close()
 }
 
 // DB Create
 func dbInsert(title string, price int) {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbInsert())")
 	}
 	db.Create(&Book{Title: title, Price: price})
+	defer db.Close()
 }
 
 // DB Update
 func dbUpdate(id int, title string, price int) {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbUpdate())")
 	}
@@ -51,63 +50,64 @@ func dbUpdate(id int, title string, price int) {
 	book.Title = title
 	book.Price = price
 	db.Save(&book)
+	db.Close()
 }
 
 // DB Delete
 func dbDelete(id int) {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbDelete())")
 	}
 	var book Book
 	db.First(&book, id)
-	db.Delete(&book)
+	db.Unscoped().Delete(&book)
+	db.Close()
 }
 
 // DB All Get
 func dbGetAll() []Book {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbGetAll())")
 	}
 	var books []Book
 	db.Order("created_at desc").Find(&books)
+	db.Close()
 	return books
 }
 
 // DB One Get
 func dbGetOne(id int) Book {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbGetOne())")
 	}
 	var book Book
 	db.First(&book, id)
+	db.Close()
 	return book
 }
 
 func dbGetNum() int {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbGetNum())")
 	}
 	var num int
 	db.Table("books").Count(&num)
+	db.Close()
 	return num
 }
 
 func dbGetPrice() Result {
 	db, err := gorm.Open("sqlite3", "book.sqlite3")
-	defer db.Close()
 	if err != nil {
 		panic("You can't open DB (dbGetPrice())")
 	}
 	var result Result
 	db.Table("books").Select("sum(price) as total").Scan(&result)
+	db.Close()
 	return result
 }
 
